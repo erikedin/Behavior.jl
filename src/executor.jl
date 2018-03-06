bc171688ddc1d896e2e3457d2c98838bc9e32bb5
Erik Edin <erikedin@users.noreply.github.com>
2018-03-06 08:10:01 +0100

    another test for the executor

--- src/executor.jl
+++ src/executor.jl
@@ -1,13 +1,25 @@
-struct Executor end
+abstract type StepDefinitionMatcher end
+
+struct Executor
+    stepdefmatcher::StepDefinitionMatcher
+end
 
 struct StepExecutionResult
     v::Int
 end
 
 const NoStepDefinitionFound = StepExecutionResult(0)
+const SuccessfulStepExecution = StepExecutionResult(1)
 
 struct ScenarioResult
     steps::Vector{StepExecutionResult}
 end
 
-executescenario(::Executor, ::Gherkin.Scenario) = ScenarioResult([NoStepDefinitionFound])
+function executescenario(executor::Executor, scenario::Gherkin.Scenario)
+    steps = if isempty(executor.stepdefmatcher.steps)
+        NoStepDefinitionFound
+    else
+        SuccessfulStepExecution
+    end
+    ScenarioResult([steps])
+end
--- test/executor_test.jl
+++ test/executor_test.jl
@@ -1,13 +1,32 @@
 using Base.Test
 using BDD.Gherkin
+using BDD
+
+successful_step_definition() = nothing
+
+struct FakeStepDefinitionMatcher <: BDD.StepDefinitionMatcher
+    steps::Dict{BDD.Gherkin.ScenarioStep, Function}
+end
 
 @testset "Executor        " begin
     @testset "Execute a one-step scenario; No matching step found; Result is NoStepDefinitionFound" begin
-        executor = BDD.Executor()
+        stepdefmatcher = FakeStepDefinitionMatcher(Dict{BDD.Gherkin.ScenarioStep, Function}())
+        executor = BDD.Executor(stepdefmatcher)
         scenario = Scenario("Description", [], [Given("some precondition")])
 
         scenarioresult = BDD.executescenario(executor, scenario)
 
         @test scenarioresult.steps[1] == BDD.NoStepDefinitionFound
+    end
+
+    @testset "Execute a one-step scenario; The matching step is successful; Result is Successful" begin
+        given = Given("Some precondition")
+        stepdefmatcher = FakeStepDefinitionMatcher(Dict(given => successful_step_definition))
+        executor = BDD.Executor(stepdefmatcher)
+        scenario = Scenario("Description", [], [given])
+
+        scenarioresult = BDD.executescenario(executor, scenario)
+
+        @test scenarioresult.steps[1] == BDD.SuccessfulStepExecution
     end
 end
