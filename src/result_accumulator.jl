4a4cb830de19d189870cba6a13f5b8b3eb70bccc
Erik Edin <erikedin@users.noreply.github.com>
2018-04-01 09:07:44 +0200

    accumulator: find accumulated result

--- src/result_accumulator.jl
+++ src/result_accumulator.jl
@@ -1,4 +1,14 @@
-struct ResultAccumulator end
+mutable struct ResultAccumulator
+    isaccumsuccess::Bool
 
-accumulateresult(::ResultAccumulator, ::FeatureResult) = nothing
-issuccess(::ResultAccumulator) = true
+    ResultAccumulator() = new(true)
+end
+
+function accumulateresult(acc::ResultAccumulator, result::FeatureResult)
+    for scenarioresult in result.scenarioresults
+        arestepssuccessful = [issuccess(step) for step in scenarioresult.steps]
+        acc.isaccumsuccess &= all(arestepssuccessful)
+    end
+end
+
+issuccess(acc::ResultAccumulator) = acc.isaccumsuccess
--- test/result_accumulator_test.jl
+++ test/result_accumulator_test.jl
@@ -1,6 +1,6 @@
 using ExecutableSpecifications:
     ResultAccumulator, accumulateresult, issuccess,
-    FeatureResult, ScenarioResult, Given, SuccessfulStepExecution, Scenario
+    FeatureResult, ScenarioResult, Given, SuccessfulStepExecution, Scenario, StepFailed
 
 @testset "Result Accumulator" begin
     @testset "Accumulate results; One feature with a successful scenario; Total result is success" begin
@@ -13,5 +13,44 @@ using ExecutableSpecifications:
         accumulateresult(accumulator, featureresult)
 
         @test issuccess(accumulator)
+    end
+
+    @testset "Accumulate results; One feature with a failing scenario; Total result is fail" begin
+        accumulator = ResultAccumulator()
+
+        given = Given("some precondition")
+        scenario = ScenarioResult([StepFailed()], Scenario("Some scenario", [], [given]))
+        featureresult = FeatureResult([scenario])
+
+        accumulateresult(accumulator, featureresult)
+
+        @test issuccess(accumulator) == false
+    end
+
+    @testset "Accumulate results; One scenario with one successful and one failing step; Total result is fail" begin
+        accumulator = ResultAccumulator()
+
+        given = Given("some precondition")
+        when = When("some action")
+        scenario = ScenarioResult([SuccessfulStepExecution(), StepFailed()], Scenario("Some scenario", [], [given, when]))
+        featureresult = FeatureResult([scenario])
+
+        accumulateresult(accumulator, featureresult)
+
+        @test issuccess(accumulator) == false
+    end
+
+    @testset "Accumulate results; Two scenarios, one failing; Total result is fail" begin
+        accumulator = ResultAccumulator()
+
+        given = Given("some precondition")
+        when = When("some action")
+        successfulscenario = ScenarioResult([SuccessfulStepExecution()], Scenario("Some scenario", [], [given]))
+        failingscenario = ScenarioResult([StepFailed()], Scenario("Some other scenario", [],  [when]))
+        featureresult = FeatureResult([successfulscenario, failingscenario])
+
+        accumulateresult(accumulator, featureresult)
+
+        @test issuccess(accumulator) == false
     end
 end
