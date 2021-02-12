34f84c6eeae8c670ffb907565a2be0c57765c542
Erik Edin <erikedin@users.noreply.github.com>
2021-02-12 21:46:17 +0100

    issue #46: additional tests for data tables

--- test/gherkin/data_tables_test.jl
+++ test/gherkin/data_tables_test.jl
@@ -25,4 +25,57 @@ using ExecutableSpecifications.Gherkin: issuccessful, parsescenario!, Given, Whe
             ["foo 3", "bar 3"],
         ]
     end
+
+    @testset "A data table with comments in the elements; Comments are ignored" begin
+        text = """
+        Scenario: Data tables
+            When some action
+            Then some tabular data
+                | header 1 | header 2 |
+                | foo 1    | bar 1    |
+                | foo 2    | bar 2    |
+                # Comment
+                | foo 3    | bar 3    |
+        """
+
+        byline = ByLineParser(text)
+        result = parsescenario!(byline)
+
+        @test issuccessful(result)
+        scenario = result.value
+
+        @test scenario.steps[2].datatable == [
+            ["header 1", "header 2"],
+            ["foo 1", "bar 1"],
+            ["foo 2", "bar 2"],
+            ["foo 3", "bar 3"],
+        ]
+    end
+
+    @testset "A data table with blank lines; Blank lines are ignored" begin
+        text = """
+        Scenario: Data tables
+            When some action
+            Then some tabular data
+                | header 1 | header 2 |
+                | foo 1    | bar 1    |
+
+                | foo 2    | bar 2    |
+
+                | foo 3    | bar 3    |
+        """
+
+        byline = ByLineParser(text)
+        result = parsescenario!(byline)
+
+        @test issuccessful(result)
+        scenario = result.value
+
+        @test scenario.steps[2].datatable == [
+            ["header 1", "header 2"],
+            ["foo 1", "bar 1"],
+            ["foo 2", "bar 2"],
+            ["foo 3", "bar 3"],
+        ]
+    end
 end
