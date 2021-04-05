4f25b5888ab5fb2b9eb4d90cfb98c84fc1dc7865
Erik Edin <erikedin@users.noreply.github.com>
2021-04-05 09:36:22 +0200

    fix the examples

--- example/features/steps/steps.jl
+++ example/features/steps/steps.jl
@@ -1,39 +1,39 @@
 using ExecutableSpecifications: @given, @when, @then, @expect
 
-@given "that the context variable :x has value 1" begin
+@given("that the context variable :x has value 1") do context
     context[:x] = 1
 end
 
-@when "the context variable :x is compared with 1" begin
+@when("the context variable :x is compared with 1") do context
     v = context[:x] == 1
     context[:result] = v
 end
 
-@then "the comparison is true" begin
+@then("the comparison is true") do context
     @expect context[:result]
 end
 
-@when "this step fails" begin
+@when("this step fails") do context
     @expect 1 == 2
 end
 
-@then "this step is skipped" begin
+@then("this step is skipped") do context
     @assert false "This code is never executed, because the previous step failed."
 end
 
-@when "this step throws an exception, the result is \"Unknown exception\"" begin
+@when("this step throws an exception, the result is \"Unknown exception\"") do context
     throw(ErrorException("This is an unknown exception"))
 end
 
 # This step is commented to show what happens when a step definition is not found.
-#@given "a step that has no corresponding step definition in steps/spec.jl" begin
+#@given("a step that has no corresponding step definition in steps/spec.jl") do context
 #
 #end
 
-@when "it is executed" begin
+@when("it is executed") do context
     @assert false "This step should be skipped, because the previous failed."
 end
 
-@then "it fails with error \"No matching step\"" begin
+@then("it fails with error \"No matching step\"") do context
     @assert false "This step should be skipped, because a previous step failed."
 end
--- example/features/steps/steps2.jl
+++ example/features/steps/steps2.jl
@@ -1,49 +1,49 @@
 using ExecutableSpecifications: @given, @when, @then, @expect
 
-@given "some precondition which does nothing" begin
+@given("some precondition which does nothing") do context
 
 end
 
-@when "we perform a no-op step" begin
+@when("we perform a no-op step") do context
 
 end
 
-@then "the scenario as a whole succeeds" begin
+@then("the scenario as a whole succeeds") do context
 
 end
 
-@when "a step has more than one matching step definition" begin
+@when("a step has more than one matching step definition") do context
 
 end
 
-@when "a step has more than one matching step definition" begin
+@when("a step has more than one matching step definition") do context
 
 end
 
-@when "foo is set to value 42" begin
+@when("foo is set to value 42") do context
     context[:foo] = 42
 end
 
-@when "foo is set to value -17" begin
+@when("foo is set to value -17") do context
     context[:foo] = -17
 end
 
-@then "foo is greater than zero" begin
+@then("foo is greater than zero") do context
     @expect context[:foo] > 0
 end
 
-@then "foo is less than zero" begin
+@then("foo is less than zero") do context
     @expect context[:foo] < 0
 end
 
-@when "we need multiple lines of text" begin
+@when("we need multiple lines of text") do context
     docstring = context[:block_text]
     @expect docstring == """
         This is line 1.
         This is line 2."""
 end
 
-@then "we can use doc strings like this" begin
+@then("we can use doc strings like this") do context
     docstring = context[:block_text]
     @expect docstring == """
         And like
