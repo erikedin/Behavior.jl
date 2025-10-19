e02d9cab3e1215b761f3db3a236b183bae6cfac0
Erik Edin <erikedin@users.noreply.github.com>
2025-10-19 13:51:08 +0200

    issue #128: move Rule parser tests to separate file

---
+++ test/gherkin/experimental/rules_parser_test.jl
@@ -0,0 +1,158 @@
+# Copyright 2018 Erik Edin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+@testset "RuleParser           " begin
+    @testset "Empty Rule, no description; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule:
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == ""
+        @test result.value.scenarios == []
+    end
+
+    @testset "Empty Rule, Some rule description; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule: Some rule description
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == "Some rule description"
+        @test result.value.scenarios == []
+    end
+
+    @testset "Rule with one scenario; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule: Some rule description
+                Scenario: Some scenario
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == "Some rule description"
+        @test result.value.scenarios[1].description == "Some scenario"
+    end
+
+    @testset "Rule with one example; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule: Some rule description
+                Example: Some scenario
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == "Some rule description"
+        @test result.value.scenarios[1].description == "Some scenario"
+    end
+
+    @testset "Rule with two scenarios; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule: Some rule description
+                Scenario: Some scenario
+                Scenario: Some other scenario
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == "Some rule description"
+        @test result.value.scenarios[1].description == "Some scenario"
+        @test result.value.scenarios[2].description == "Some other scenario"
+    end
+
+    @testset "Rule with two scenarios any steps; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule: Some rule description
+                Scenario: Some scenario
+                    Given some precondition
+                    Given some other precondition
+                Scenario: Some other scenario
+                    Given some third precondition
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == "Some rule description"
+        @test result.value.scenarios[1].description == "Some scenario"
+        @test result.value.scenarios[1].steps == [
+            Given("some precondition"), Given("some other precondition")
+        ]
+        @test result.value.scenarios[2].description == "Some other scenario"
+        @test result.value.scenarios[2].steps == [
+            Given("some third precondition")
+        ]
+    end
+
+    @testset "Rule with two scenarios any steps separated by blank lines; OK" begin
+        # Arrange
+        input = ParserInput("""
+            Rule: Some rule description
+
+                Scenario: Some scenario
+                    Given some precondition
+                    Given some other precondition
+
+                Scenario: Some other scenario
+                    Given some third precondition
+        """)
+
+        # Act
+        parser = RuleParser()
+        result = parser(input)
+
+        # Assert
+        @test result isa OKParseResult{Rule}
+        @test result.value.description == "Some rule description"
+        @test result.value.scenarios[1].description == "Some scenario"
+        @test result.value.scenarios[1].steps == [
+            Given("some precondition"), Given("some other precondition")
+        ]
+        @test result.value.scenarios[2].description == "Some other scenario"
+        @test result.value.scenarios[2].steps == [
+            Given("some third precondition")
+        ]
+    end
+end
